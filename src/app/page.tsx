4852a4ac3c01b7190dd3a5de211e03f0ae245a29
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2024-12-10 06:55:54 -0300

    feat: create basic homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-"use client";
-import { Button, Flex, Text } from "@radix-ui/themes";
+import { Button, Card, Container, Flex, Text } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-    <Flex direction="column" gap="2">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
-    </Flex>
+    <Container>
+      <Card>
+        <Flex direction="column" gap="2">
+          <Text>Hello from Radix Themes :)</Text>
+          <Button>Let's go</Button>
+        </Flex>
+      </Card>
+    </Container>
   );
 }
