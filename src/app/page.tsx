01fe6120ac323d593c02149c858d00a1cc21d40b
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2024-12-11 06:10:26 -0300

    feat: refactor and list todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
-import { Button, Card, Container, Flex, Text } from "@radix-ui/themes";
+'use client'
+
+import { Card, Container, Flex, Text, TextField } from "@radix-ui/themes";
+import { KeyboardEvent, useState, ChangeEvent } from "react";
 
 export default function Home() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState<string[]>([])
+
+  function addNewTodo(newTodo: string) {
+    setTodoList(prev => [...prev, newTodo]);
+    setTodo("")
+  }
+
+  function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      return addNewTodo(todo)
+    }
+  }
+
+  function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
+    setTodo(e.target.value)
+  }
+
   return (
     <Container>
       <Card>
         <Flex direction="column" gap="2">
-          <Text>Hello from Radix Themes :)</Text>
-          <Button>Let's go</Button>
+          <TextField.Root value={todo} onChange={handleOnChange} placeholder="Add a todo" onKeyDown={handleKeyDown}/>
+          {todoList.map((value) => {
+            return <Text>{value}</Text>
+          })}
         </Flex>
       </Card>
     </Container>
